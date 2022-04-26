cf88bb2aaebda58e8207b8112ed04728643a9ae5
Osman Tuzcu <osmantuzcu@gmail.com>
2022-04-26 20:57:15 +0300

    contract

--- public/scripts/const.js
+++ public/scripts/const.js
@@ -1,3 +1,3 @@
 export const wchainId = 0xa86a
-export const dwarfContractAddress = "0xc63d5fBAe773d1d52E98e955cd689c3eaf0113FF"
+export const dwarfContractAddress = "0x0a8CE38791ff9D6c5e8D8E20c13A7c939098936E"
 export const passContractAdress = "0x29267697d2ff7Eff7cD83175a109ADbA7aA7A91f"
